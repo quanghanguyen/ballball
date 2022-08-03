3aa92a684a7ed638c3f3146bdbb0da44f4d58d5a
Neo <hakun32@gmail.com>
2022-08-03 10:16:29 +0700

    c3-0308

--- app/src/main/java/com/example/ballball/user/userinfomation/UserInformationActivity.kt
+++ app/src/main/java/com/example/ballball/user/userinfomation/UserInformationActivity.kt
@@ -1,12 +1,23 @@
 package com.example.ballball.user.userinfomation
 
+import android.Manifest
+import android.annotation.SuppressLint
 import android.content.Intent
+import android.content.pm.PackageManager
+import android.graphics.Bitmap
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.provider.MediaStore
+import android.widget.Toast
 import androidx.activity.viewModels
+import androidx.core.app.ActivityCompat
+import com.example.ballball.R
 import com.example.ballball.databinding.ActivityUserInformationBinding
+import com.example.ballball.databinding.LayoutBottomSheetDialogBinding
 import com.example.ballball.user.teaminformation.TeamInformationActivity
 import com.example.ballball.utils.Animation
+import com.google.android.material.bottomsheet.BottomSheetDialog
 import com.google.firebase.auth.FirebaseAuth
 import dagger.hilt.android.AndroidEntryPoint
 import java.io.File
@@ -18,6 +29,8 @@ class UserInformationActivity : AppCompatActivity() {
     private val userInformationViewModel : UserInformationViewModel by viewModels()
     private val userUID = FirebaseAuth.getInstance().currentUser?.uid
     private val localFile = File.createTempFile("tempImage", "jpg")
+    private lateinit var layoutBottomSheetDialogBinding: LayoutBottomSheetDialogBinding
+    private lateinit var imgUri : Uri
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -36,6 +49,7 @@ class UserInformationActivity : AppCompatActivity() {
     private fun initEvents() {
         back()
         teamInformation()
+        editAvatar()
     }
 
     private fun teamInformation() {
@@ -52,9 +66,78 @@ class UserInformationActivity : AppCompatActivity() {
         }
     }
 
+    private fun editAvatar() {
+        userInformationBinding.editProfilePictureButton.setOnClickListener {
+            showBottomSheetDialog()
+        }
+    }
+
+    private fun showBottomSheetDialog() {
+        val dialog = BottomSheetDialog(this, R.style.CustomBottomSheetDialog)
+        layoutBottomSheetDialogBinding = LayoutBottomSheetDialogBinding.inflate(layoutInflater)
+        dialog.setContentView(layoutBottomSheetDialogBinding.root)
+
+        layoutBottomSheetDialogBinding.takePhoto.setOnClickListener {
+
+            if (ActivityCompat.checkSelfPermission(applicationContext, Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED) {
+                ActivityCompat.requestPermissions(this, arrayOf(Manifest.permission.CAMERA), 100)
+            } else {
+                selectAvatarFromCamera()
+                dialog.dismiss()
+                Animation.animateFade(this)
+            }
+        }
+
+        layoutBottomSheetDialogBinding.gallery.setOnClickListener{
+            selectAvatar()
+            dialog.dismiss()
+            Animation.animateFade(this)
+        }
+        dialog.show()
+    }
+
+    private fun selectAvatar() {
+        val intent = Intent()
+        intent.type = "image/*"
+        intent.action = Intent.ACTION_GET_CONTENT
+        startActivityForResult(intent, 0)
+    }
+
+    @SuppressLint("QueryPermissionsNeeded")
+    private fun selectAvatarFromCamera() {
+        val intent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
+        if (intent.resolveActivity(packageManager) != null) {
+            startActivityForResult(intent, 1)
+        } else {
+            Toast.makeText(this, "Camera không hoạt động", Toast.LENGTH_SHORT).show()
+        }
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+
+        if (requestCode == 0 && resultCode == RESULT_OK) {
+            imgUri = data?.data!!
+            userInformationBinding.profilePicture.setImageURI(imgUri)
+        }
+
+        if (requestCode == 1 && resultCode == RESULT_OK)  {
+            val bundle : Bundle? = data?.extras
+            val finalPhoto : Bitmap = bundle?.get("data") as Bitmap
+            userInformationBinding.profilePicture.setImageBitmap(finalPhoto)
+        }
+
+        if (this::imgUri.isInitialized) {
+            if (userUID != null) {
+                userInformationViewModel.saveAvatar(imgUri, userUID)
+            }
+        }
+    }
+
     private fun initObserve() {
         loadAvatarObserver()
         loadNameAndPhoneObserve()
+        saveAvatarObserve()
     }
 
     private fun loadAvatarObserver() {
@@ -76,6 +159,17 @@ class UserInformationActivity : AppCompatActivity() {
                     userInformationBinding.userPhoneNumber.text = result.userPhone
                 }
                 is UserInformationViewModel.LoadNameAndPhone.ResultError -> {}
+            }
+        }
+    }
+
+    private fun saveAvatarObserve() {
+        userInformationViewModel.saveAvatar.observe(this) {result ->
+            when (result) {
+                is UserInformationViewModel.SaveAvatar.ResultOk -> {}
+                is UserInformationViewModel.SaveAvatar.ResultError -> {
+                    Toast.makeText(this, result.errorMessage, Toast.LENGTH_SHORT).show()
+                }
             }
         }
     }
--- app/src/main/java/com/example/ballball/user/userinfomation/UserInformationViewModel.kt
+++ app/src/main/java/com/example/ballball/user/userinfomation/UserInformationViewModel.kt
@@ -1,10 +1,13 @@
 package com.example.ballball.user.userinfomation
 
 import android.graphics.Bitmap
+import android.net.Uri
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.example.ballball.main.home.HomeRepository
+import com.example.ballball.user.walkthrough.avatar.AvatarRepository
+import com.example.ballball.user.walkthrough.avatar.AvatarViewModel
 import com.google.firebase.database.DataSnapshot
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.CoroutineExceptionHandler
@@ -15,11 +18,13 @@ import javax.inject.Inject
 @HiltViewModel
 class UserInformationViewModel @Inject constructor(
     private val homeRepository : HomeRepository,
-    private val userInformationRepository : UserInformationRepository
+    private val userInformationRepository : UserInformationRepository,
+    private val avatarRepository: AvatarRepository
     ) : ViewModel() {
 
     val loadAvatar = MutableLiveData<LoadAvatar>()
     val loadNameAndPhone = MutableLiveData<LoadNameAndPhone>()
+    val saveAvatar = MutableLiveData<SaveAvatar>()
 
     sealed class LoadAvatar {
         class ResultOk(val image : Bitmap) : LoadAvatar()
@@ -29,6 +34,11 @@ class UserInformationViewModel @Inject constructor(
     sealed class LoadNameAndPhone {
         class ResultOk(val userName : String, val userPhone : String) : LoadNameAndPhone()
         object ResultError : LoadNameAndPhone()
+    }
+
+    sealed class SaveAvatar {
+        object ResultOk : SaveAvatar()
+        class ResultError(val errorMessage : String) : SaveAvatar()
     }
 
     fun loadAvatar(
@@ -58,6 +68,18 @@ class UserInformationViewModel @Inject constructor(
                 }
             }, {
                 loadNameAndPhone.value = LoadNameAndPhone.ResultError
+            })
+        }
+    }
+
+    fun saveAvatar(imgUri : Uri, userUID : String) {
+        viewModelScope.launch(CoroutineExceptionHandler { _, throwable ->
+            throwable.printStackTrace()
+        }) {
+            avatarRepository.saveAvatar(imgUri, userUID, {
+                saveAvatar.value = SaveAvatar.ResultOk
+            }, {
+                saveAvatar.value = SaveAvatar.ResultError(it)
             })
         }
     }
